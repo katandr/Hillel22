09643bdcebc3eacc8661129387ea1b074e594d78
macbook <kate@salatova.com.ua>
2022-12-11 19:41:59 +0000

    Merge remote-tracking branch 'origin/main'

--- homework5.py
+++ homework5.py
@@ -1,3 +1,20 @@
+# 1)У вас есть строка my_string = '0123456789'.
+# Сгенерировать целые числа (тип int) от 0 до 99 и распечатать их.
+# Задание нужно выполнить ТОЛЬКО через цикл в цикле(См. пример выше) и приведение типов.
+
+my_string = '0123456789'
+i = 0
+y = 0
+while i < 10:
+    first = my_string[i]
+    i+=1
+    while y < 10:
+        second = my_string[y]
+        print(first+second)
+        y += 1
+    y=0
+
+
 # При помощи вложенных циклов (можно while, можно for) нарисовать следующие фигуры
 # представленные ниже. Пользователь вводит, с клавиатуры, высоту фигуры в символах.
 # height = input("введите высоту ")
@@ -9,125 +26,3 @@
 #     *               *
 #   *                   *
 # * * * * * * * * * * * * *
-height=int(input("введите высоту="))
-rows = height -1
-print("========Figure 1=========")
-for x in range(0,rows):
-    for y in range(x,rows):
-         print(" ", end=" ")
-
-    print("*", end=" ")
-
-    for t in range((x*2-1)):
-        print(" ", end=" ")
-    if x>0:
-        print("*", end=" ")
-
-    for i in range(x,rows):
-        print(" ", end=" ")
-    print(" ")
-
-    if x == rows-1:
-        for j in range(rows*2+1):
-            print("*", end=" ")
-
-print("\n\n========Figure 2=========")
-for x in range(0,rows):
-    for y in range(x,rows):
-         print(" ", end=" ")
-
-    print("*", end=" ")
-
-    for t in range((x*2-1)):
-        print("*", end=" ")
-    if x>0:
-        print("*", end=" ")
-
-    for i in range(x,rows):
-        print(" ", end=" ")
-    print(" ")
-
-    if x == rows-1:
-        for j in range(rows*2+1):
-            print("*", end=" ")
-
-
-print("\n\n========Figure 3=========")
-
-for x in range(0,rows):
-    for y in range(x,rows):
-         print(" ", end=" ")
-
-    print("*", end=" ")
-
-    for t in range((x*2-1)):
-        print("*", end=" ")
-    if x>0:
-        print("*", end=" ")
-
-    for i in range(x,rows):
-        print(" ", end=" ")
-    print(" ")
-
-    if x == rows-1:
-        for j in range(rows*2+1):
-            print("*", end=" ")
-print("")
-
-for x in range(rows,0,-1):
-    for y in range(x,rows+1):
-         print(" ", end=" ")
-
-    print("*", end=" ")
-
-    for t in range((x*2-3)):
-        print(" ", end=" ")
-    if x !=1:
-        print("*", end=" ")
-
-    for i in range(x,rows+1):
-        print(" ", end=" ")
-    print(" ")
-
-
-print("\n\n========Figure 4=========")
-
-for x in range(0,rows):
-    for y in range(x,rows):
-         print(" ", end=" ")
-
-    print("*", end=" ")
-
-    for t in range((x*2-1)):
-        print("*", end=" ")
-    if x>0:
-        print("*", end=" ")
-
-    for i in range(x,rows):
-        print(" ", end=" ")
-    print(" ")
-
-    if x == rows-1:
-        for j in range(rows*2+1):
-            print("*", end=" ")
-print("")
-
-for x in range(rows,0,-1):
-    for y in range(x,rows+1):
-         print(" ", end=" ")
-
-    print("*", end=" ")
-
-    for t in range((x*2-3)):
-        if t == x-2:
-            print("*", end=" ")
-        else:
-            print(" ", end=" ")
-
-    if x !=1:
-        print("*", end=" ")
-
-    for i in range(x,rows+1):
-        print(" ", end=" ")
-    print(" ")
-
