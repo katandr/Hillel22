3231932cb7b50c6b82609b8ba19059ad6c164be5
macbook <kate@salatova.com.ua>
2022-12-25 11:21:52 +0000

    Merge remote-tracking branch 'origin/main'

--- homework7.py
+++ homework7.py
@@ -3,13 +3,31 @@
 # my_list_1, а потом все элементы на нечетных местах из my_list_2.
 
 from random import randint
+my_result = []
+my_list_1 = [randint(1,100) for x in range(10)]
+my_list_2 = [randint(1,100) for x in range(10)]
+my_result = my_list_2
+print("my_list_1",my_list_1)
+print("my_list_2",my_list_2)
 
-my_list_1=[]
-my_list_2=[]
+for i in range (1,20,2):
+    my_result.insert(i,my_list_1.pop(0))
+print(my_result)
 
-for i in range(10):
-    my_list_1[i] = randint(1,400)
-    my_list_2[i] = randint(1,400)
-print(my_list_1)
-print(my_list_2)
+#
+# 11. Дана строка my_str. Создать список в который поместить те символы из my_str, которые встречаются
+# в строке ТОЛЬКО ОДИН раз.
+
+my_str = "this is the the first and the last string number twenty"
+list=[]
+count=0
+for i in range(len(my_str)):
+    count = 0
+    for y in range(len(my_str)):
+        if my_str[i] == my_str[y]:
+            count+=1
+    if count == 1:
+        list.append(my_str[i])
+
+print(list)
 
