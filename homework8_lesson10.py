7ac263539e9c9e9ef89c2107f43e475d154cef10
macbook <kate@salatova.com.ua>
2023-01-08 09:15:54 +0000

    Merge remote-tracking branch 'origin/main'

--- homework8_lesson10.py
+++ homework8_lesson10.py
@@ -1,9 +1,10 @@
 import homework8_lesson10_module as hw8
 
-my_list = ['qwe','ett','der','frr','hrt','axc']
+my_list = ['qwe','ett','aer','frr','art','axc']
 
 def main():
     hw8.string_odd_change(my_list)
+    hw8.first_a(my_list)
 main()
 
 
--- homework8_lesson10_module.py
+++ homework8_lesson10_module.py
@@ -12,7 +12,7 @@ def string_odd_change(my_list) -> list:
         Если на четном - оставить без изменения
         :return: new string
         """
-    print(f'initial string     {my_list}')
+    print(f'initial list     {my_list}')
     new_list=[]
     for i in range(len(my_list)):
         if i % 2 == 0:
@@ -21,7 +21,20 @@ def string_odd_change(my_list) -> list:
 
         else:
             new_list.append(my_list[i])
-    print(f'transformed string {new_list}')
+    print(f'transformed list {new_list}')
     return(new_list)
 
+def first_a(my_list) -> list:
+    """
+        Функция возвращает новый список в котором содержаться
+элементы из my_list у которых первый символ - буква "a".
+        :return: new string
+    """
+    print(f'initial list {my_list}')
+    new_list=[]
+    for i in my_list:
+        if i.find('a') == 0:
+            new_list.append(i)
 
+    print(f'new list {new_list}')
+    return (new_list)
