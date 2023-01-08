d64af7d3e1e9be1bbe03f92e16a6d2c758935ee8
macbook <kate@salatova.com.ua>
2023-01-08 13:24:51 +0000

    Merge remote-tracking branch 'origin/main'

--- homework8_lesson10.py
+++ homework8_lesson10.py
@@ -1,10 +1,29 @@
 import homework8_lesson10_module as hw8
 
-my_list = ['qwe','ett','aer','frr','art','axc']
-
+my_list = ['qwae','ett','aer','frr','art','axc']
+my_list2 = ['qwae','ett',456, 56,'aer','frr',67, 'art','axc']
+my_str = 'dtyuytrtyunasas'
+my_str1 = 'qwrtypk'
+my_str2 = 'qaqqwolpp'
+names = ['smith','robinson','black','bush','klinton']
+domains = ['google','yahoo','microsoft','tiktok']
 def main():
-    hw8.string_odd_change(my_list)
-    hw8.first_a(my_list)
+    print(f'transformed list {hw8.string_odd_change(my_list)}')
+    print("")
+    print(f'new list with string begin with a {hw8.first_a(my_list)}')
+    print("")
+    print(f'new list with a in the string {hw8.anywhere_a(my_list)}')
+    print("")
+    print(f'new list with strings only {hw8.only_strings(my_list2)}')
+    print("")
+    print(f'new list with unique char {hw8.unique_chars(my_str)}')
+    print("")
+    print(f'new list with unique char {hw8.compare_strings(my_str1,my_str2)}')
+    print("")
+    print(f'new list with unique char {hw8.compare_strings_unque_char(my_str1, my_str2)}')
+    print("")
+    print(f'generated email: {hw8.generate_email(names,domains)}')
+
 main()
 
 
--- homework8_lesson10_module.py
+++ homework8_lesson10_module.py
@@ -1,9 +1,5 @@
-# 1. Написать функцию которой передается один параметр - список строк my_list.
-# Функция возвращает новый список в котором содержаться
-# элементы из my_list по следующему правилу:
-# Если строка стоит на нечетном месте в my_list, то ее заменить на
-# перевернутую строку. "qwe" на "ewq".
-# Если на четном - оставить без изменения.
+import random
+import string
 
 def string_odd_change(my_list) -> list:
     """
@@ -21,7 +17,6 @@ def string_odd_change(my_list) -> list:
 
         else:
             new_list.append(my_list[i])
-    print(f'transformed list {new_list}')
     return(new_list)
 
 def first_a(my_list) -> list:
@@ -36,5 +31,110 @@ def first_a(my_list) -> list:
         if i.find('a') == 0:
             new_list.append(i)
 
-    print(f'new list {new_list}')
     return (new_list)
+
+def anywhere_a(my_list) -> list:
+    """
+        Функция возвращает новый список в котором содержаться
+элементы из my_list в которых есть символ - буква "a" на любом месте.
+        :return: new string
+    """
+    print(f'initial list {my_list}')
+    new_list=[]
+    for i in my_list:
+        if 'a'in i:
+            new_list.append(i)
+    return (new_list)
+
+def only_strings(my_list) -> list:
+    """
+        Функция возвращает новый список в котором содержаться только строки из my_list.
+        :return: new string
+    """
+    print(f'initial list {my_list}')
+    new_list=[]
+    for i in my_list:
+        if type(i) == str:
+            new_list.append(i)
+    return (new_list)
+
+def unique_chars(my_str) -> list:
+    """
+    Функция возвращает новый список в котором содержаться те символы из my_str,
+    которые встречаются в строке только один раз.
+    """
+    print(f'initial string {my_str}')
+    new_list=[]
+    for char in my_str:
+        count=0
+        for i in my_str:
+            if char == i:
+                count+=1
+        if count == 1:
+            new_list.append(char)
+    return new_list
+
+def compare_strings(my_str1:str, my_str2:str) -> list:
+    """
+    Функция возвращает список в который поместить те символы,
+которые есть в обеих строках хотя бы раз.
+    :param my_str1: 
+    :param my_str2: 
+    :return: new string
+    """
+    print(f'initial string 1 {my_str1}')
+    print(f'initial string 2 {my_str2}')
+    new_list = []
+    for i in my_str1:
+        count=0
+        for j in my_str2:
+            if i==j:
+                count+=1
+        if count>0:
+            new_list.append(i)
+    return new_list
+
+def compare_strings_unque_char(my_str1:str,my_str2:str) ->list:
+    """
+    Функция возвращает список в который поместить те символы, которые есть в обеих строках,
+но в каждой только по одному разу.
+    :param my_str1: 
+    :param my_str2: 
+    :return: new list
+    """
+    print(f'initial string 1 {my_str1}')
+    print(f'initial string 2 {my_str2}')
+    new_list = []
+    for i in my_str1:
+        count = 0
+        for j in my_str2:
+            if i == j:
+                count += 1
+        if count == 1:
+            new_list.append(i)
+    return new_list
+
+def generate_email(names:list,domains:list) ->str:
+    """
+    функцию для генерирования e-mail в формате:
+фамилия.число_от_100_до_999@строка_букв_длинной_от_5_до_7_символов.домен
+фамилию и домен брать случайным образом из заданных списков переданных в функцию в виде параметров.
+Строку и число генерировать случайным образом.
+
+    :param names:
+    :param domains:
+    :return: generated email as string
+    """
+
+    email = ''
+    random_string = ''
+    number_of_names = len(names)
+    number_of_domains = len(domains)
+    name = names[random.randrange(number_of_names)]
+    domain = domains[random.randrange(number_of_domains)]
+    number = random.randrange(100,999)
+    random_string =''.join(random.choice(string.ascii_lowercase) for i in range(random.randrange(5,8)))
+
+    email = name+'.'+str(number)+'@'+random_string+'.'+domain
+
+    return email
