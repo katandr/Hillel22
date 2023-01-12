f3c2ab72dd07bd4b61dc3ac360a58c31955cedef
macbook <kate@salatova.com.ua>
2023-01-12 18:25:14 +0000

    Merge remote-tracking branch 'origin/main'

---
+++ homework10_lesson12.py
@@ -0,0 +1,7 @@
+
+def censor(file, words_to_replace:list):
+    with open(file, 'r') as file:
+        text = f.read(file)
+        for word in words_to_replace:
+
+
--- homework9_lesson11.py
+++ homework9_lesson11.py
@@ -4,7 +4,7 @@ def censor(file,replacements:list):
     with open(file, 'r') as f:
         data = f.read()
         for word in replacements:
-            print(word)
+
             data = data.replace(word,"***")
         with open("newfile.txt", 'w') as new_file:
             new_file.write(data)
