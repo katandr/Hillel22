a5bf2868c4d5f062e7b58f048af3ec67328c4e93
macbook <kate@salatova.com.ua>
2023-01-19 13:18:13 +0000

    homework 11_lesson 13

--- homework11_2_lesson13.py
+++ homework11_2_lesson13.py
@@ -18,14 +18,21 @@ class Employee:
         """method to create employee from json file"""
         with open(file,"r") as file:
             data = json.load(file)
+
         if type(data) is dict:
-            keys=data.keys()
-            for key in keys:
-                print(key)
-                self.[key] = data.setdefault(key)
-
-
-
+            keys=tuple(data.keys())
+            print(keys)
+            print(type(keys))
+            if keys != ('first_name', 'last_name', 'email', 'age', 'skills', 'people_lang', 'coding_lang'):
+                print("error with incoming data format")
+            else:
+                self.firstname = data['first_name']
+                self.lastname = data['last_name']
+                self.email = data['email']
+                self.age = data['age']
+                self.skills = data['skills']
+                self.people_lang = data['people_lang']
+                self.coding_lang = data['coding_lang']
 
 
 
@@ -33,7 +40,7 @@ person1=Employee()
 file = 'people.json'
 new_person = Employee()
 new_person.add_employee_from_json(file)
-print("yyy",new_person.firstname)
+print("imported from file person",new_person.firstname)
 
 
 
--- homework11_lesson13.py
+++ homework11_lesson13.py
@@ -74,6 +74,25 @@ class Employee:
         new_employee=Employee()
         print(type(parsed_json))
 
+    def add_employee_from_json(self,file):
+        """method to create employee from json file"""
+        with open(file,"r") as file:
+            data = json.load(file)
+
+        if type(data) is dict:
+            keys=tuple(data.keys())
+            print(keys)
+            print(type(keys))
+            if keys != ('first_name', 'last_name', 'email', 'age', 'skills', 'people_lang', 'coding_lang'):
+                print("error with incoming data format")
+            else:
+                self.firstname = data['first_name']
+                self.lastname = data['last_name']
+                self.email = data['email']
+                self.age = data['age']
+                self.skills = data['skills']
+                self.people_lang = data['people_lang']
+                self.coding_lang = data['coding_lang']
 
 default_employee={
     'firstname': 'Ivasik',
@@ -101,3 +120,7 @@ if person2.speak_language(ask_language):
     print(f'{person2.firstname} speaks {ask_language}')
 
 
+file = 'people.json'
+new_person = Employee()
+new_person.add_employee_from_json(file)
+print("imported from file person",new_person.firstname)
